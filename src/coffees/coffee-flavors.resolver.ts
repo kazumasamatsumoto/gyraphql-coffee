fc51ff4d78b8c555f1f4153ead95342491c9c3ef
kazumasamatsumoto <kazumasa.matsumoto62@gmail.com>
2024-09-23 21:43:02 +0900

    cord-first finish

--- src/coffees/coffee-flavors.resolver.ts
+++ src/coffees/coffee-flavors.resolver.ts
@@ -3,21 +3,17 @@ import { Coffee } from './entities/coffee.entity/coffee.entity';
 import { Flavor } from './entities/flavor.entity/flavor.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { FlavorsByCoffeeLoader } from './data-loader/flavors-by-coffee.loader/flavors-by-coffee.loader';
 
 @Resolver(() => Coffee)
 export class CoffeeFlavorsResolver {
   constructor(
     @InjectRepository(Flavor)
-    private readonly flavorsRepository: Repository<Flavor>,
+    private readonly flavorsByCoffeeLoader: FlavorsByCoffeeLoader,
   ) {}
 
   @ResolveField('flavors', () => [Flavor])
   async getFlavorsOfCoffee(@Parent() coffee: Coffee) {
-    return this.flavorsRepository
-      .createQueryBuilder('flavor')
-      .innerJoin('flavor.coffees', 'coffees', 'coffees.id = :coffeeId', {
-        coffeeId: coffee.id,
-      })
-      .getMany();
+    return this.flavorsByCoffeeLoader.load(coffee.id);
   }
 }
