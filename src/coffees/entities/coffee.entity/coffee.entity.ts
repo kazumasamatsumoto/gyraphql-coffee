25ae8f304f46ba931f01f9333929639349040928
kazumasamatsumoto <kazumasa.matsumoto62@gmail.com>
2024-09-23 17:45:25 +0900

    Unions and Enums

--- src/coffees/entities/coffee.entity/coffee.entity.ts
+++ src/coffees/entities/coffee.entity/coffee.entity.ts
@@ -9,6 +9,7 @@ import {
 } from 'typeorm';
 import { Flavor } from '../flavor.entity/flavor.entity';
 import { Drink } from 'src/common/interfaces/drink.interface/drink.interface';
+import { CoffeeType } from 'src/common/enums/coffee-type.enum';
 
 @Entity()
 @ObjectType({ description: 'Coffee model', implements: () => Drink })
@@ -29,4 +30,7 @@ export class Coffee implements Drink {
 
   @CreateDateColumn()
   createdAt?: Date;
+
+  @Column({ nullable: true })
+  type?: CoffeeType;
 }
--- src/drinks/drinks.resolver.ts
+++ src/drinks/drinks.resolver.ts
@@ -1,12 +1,13 @@
 import { Query, Resolver } from '@nestjs/graphql';
 import { Coffee } from 'src/coffees/entities/coffee.entity/coffee.entity';
 import { Drink } from 'src/common/interfaces/drink.interface/drink.interface';
+import { DrinksResultUnion } from 'src/common/unions/drinks-result.union';
 import { Tea } from 'src/teas/entities/tea.entity/tea.entity';
 
 @Resolver()
 export class DrinksResolver {
-  @Query(() => [Drink], { name: 'drinks' })
-  async findAll(): Promise<Drink[]> {
+  @Query(() => [DrinksResultUnion], { name: 'drinks' })
+  async findAll(): Promise<(typeof DrinksResultUnion)[]> {
     const coffee = new Coffee();
     coffee.id = 1;
     coffee.name = 'Colombia';
